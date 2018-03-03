af925aa9161ab86b544c2e918155457b99ba9246
Nick Schmidt <nick@schmidtyanimation.com>
2018-03-03 13:59:04 -0600

    got most of the guess a number done

---
+++ exercises_week_1/python_flexercise_guessanumber.py
@@ -0,0 +1,30 @@
+import random
+
+
+def number_game():
+    secret_number = random.randint(1, 10)
+    chances = 5
+    print("""I'm thinking of a number between 1 and 10,
+        can you guess it? You have {} chances.""".format(chances))
+    
+    while chances > 0:
+        user_input = (input("What is your guess? "))
+        
+        if int(user_input) == secret_number:
+            print("You got it! The secret number was {}".format(secret_number))
+            break
+        elif int(user_input) > secret_number:
+            chances -= 1
+            print("Your guess was too high! Try again, {} chances left!".format(chances))
+        elif int(user_input) < secret_number:
+            chances -= 1
+            print("Your guess was too low! Try again, {} chances left!".format(chances))
+        elif chances == 0:
+            break
+        else: 
+            print("Valid inputs are numbers 1-10 only.")
+
+number_game()
+        
+    
+
