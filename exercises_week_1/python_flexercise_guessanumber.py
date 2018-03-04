29d71ca0d89141bb86a2ca370b1e2fa36b5457f3
Nick Schmidt <nick@schmidtyanimation.com>
2018-03-04 17:07:23 -0600

    finished guess a number game

--- exercises_week_1/python_flexercise_guessanumber.py
+++ exercises_week_1/python_flexercise_guessanumber.py
@@ -4,13 +4,17 @@ import random
 def number_game():
     secret_number = random.randint(1, 10)
     chances = 5
+    
     print("""I'm thinking of a number between 1 and 10,
         can you guess it? You have {} chances.""".format(chances))
     
     while chances > 0:
         user_input = (input("What is your guess? "))
-        
-        if int(user_input) == secret_number:
+        if len(user_input) == 0 or user_input.isalpha():
+            print("Valid inputs are numbers 1-10 only.")
+        elif int(user_input) > 10:
+            print("Valid inputs are numbers 1-10 only.")
+        elif int(user_input) == secret_number:
             print("You got it! The secret number was {}".format(secret_number))
             break
         elif int(user_input) > secret_number:
@@ -21,8 +25,7 @@ def number_game():
             print("Your guess was too low! Try again, {} chances left!".format(chances))
         elif chances == 0:
             break
-        else: 
-            print("Valid inputs are numbers 1-10 only.")
+    
 
 number_game()
         
